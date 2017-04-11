b3f1e771819db9dbdf9bb8e3f775f2bb2656abd4
Jeasonweb <1072486871@qq.com>
2017-04-11 11:17:23 +0800

    单页面项目完成暂时放下，我想给自己点时间，让自己能够专心完成bootstrap项目开发

--- app/static/js/JZMain.js
+++ app/static/js/JZMain.js
@@ -11,16 +11,63 @@
  **=================================================================================
  ************************************************************************************/
 
-var app = angular.module('myApp');
+var app = angular.module('myApp',[]);
+	app.controller('myCtrl',function($scope){		
+		$scope.firstName = 'Jeason';
+		$scope.familyName = 'Zhang';		
+		
+	});
+	app.controller('includeCtr',function($http,$scope){
+		$http.get('view/welcome.html').then(function(response){
+			
+			$scope.myWelcome = response.data;			
+			
+		});		
+	});
+	app.controller('falshCtr',function($scope,$interval){
+		$scope.myHeader = 'Hello Swallow';
+		$interval(function(){
+			$scope.myHeader = ($scope.myHeader == 'Hello Swallow')?'Hello Jeason':'Hello Swallow';
+		},1000);
+		
+	});
+	//$timeout
+	app.controller('switchCtr',function($scope,$timeout){
+		$scope.myLovers = ['Swallow','Lucy','Lily'];
+		$timeout(function(){
+			
+			$scope.myLover = $scope.getRandomLover();			
+		});	
+		$scope.getRandomLover = function(){
+			var tempCount = parseInt(Math.random()*$scope.myLovers.length);
+			return $scope.myLovers[tempCount];
+			
+		}
+	});
+	//这是笔记本文本
+	app.controller("myNoteCtrl", function($scope) {
+	    $scope.message = "";
+	    $scope.left  = function() {return 100 - $scope.message.length;};
+	    $scope.clear = function() {$scope.message = "";};
+	    $scope.save  = function() {alert("Note Saved");};
+	});
+	
+	//*****************************************
+	// 闪烁的标题栏
+	//*****************************************
+	 var count=false;
+	function flashTitle(){
+		document.title= (count?"==SPA==":"***SPA***");
+		count=!count;
+	}
+	window.onload=function(){		
+		setInterval(flashTitle,300);
+	
+	}
 
-	app.controller('parentCtr',function($scope){
-		
-		$scope.firstName='Jeason';
-		$scope.familyName ='Zhang';
-		
-		
-		
-		
-	})
+	
+	
+	
+	
 	
 
